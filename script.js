ace3ab2dc8a96e070eda75977efcc8cb955d65f3
Rubayoga <rubajeeva93@gmail.com>
2025-05-10 16:10:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const appendvalue=(value)=>{
+    document.getElementById("display").value+=value;
+}
+
+
+const  cleardisplay=()=>{
+    document.getElementById("display").value="";
+}
+
+
+const  backspace=()=>{
+    let displayval=document.getElementById("display");
+    displayval.value= displayval.value.slice(0,-1);        
+
+}
+
+
+const calculatresult=()=>{
+    try {
+        document.getElementById("display").value=eval(document.getElementById("display").value);
+
+    } catch (error) {
+       document.getElementById("display").value="Error"; 
+    }
+}
+const showHistory=()=>{
+    
+}
+const historyArea = document.querySelector('.history');
